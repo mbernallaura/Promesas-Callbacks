3a934f28eb4ed4c38af6cc5e28551b900b372f7d
mbernallaura <mbernallaura@gmail.com>
2022-10-03 20:03:18 -0500

    Se vio la promesa .race( ) y como exportar metodos de otra forma

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './styles.css';
+import {promesaLenta, promesaMedia, promesaRapida} from './js/promesas';
+
+// promesaLenta.then( console.log );
+// promesaMedia.then( console.log );
+// promesaRapida.then( console.log );
+
+//*Prommise.race() = Permite ejecutar multiples promesas y obtener la respuesta de la promesa que 
+//*Se resuelva mas rapido
+//!No importa si alguna da error antes que la promesa mas rapida, lo obvia y vota la respuesta
+//!Si la promesa mas rapida tiene un error, eso es lo que nos mostrara
+Promise.race([promesaLenta, promesaMedia, promesaRapida])
+        .then(console.log);
--- src/js/promesas.js
+++ src/js/promesas.js
@@ -29,3 +29,21 @@ export const buscarHeroe = ( heroeId ) =>{
         }
     });
 }
+
+const promesaLenta = new Promise((resolve, reject) =>{
+    setTimeout(() => resolve('Promesa Lenta'), 2000);
+});
+
+const promesaMedia = new Promise((resolve, reject) =>{
+    setTimeout(() => resolve('Promesa Media'), 1500);
+});
+
+const promesaRapida = new Promise((resolve, reject) =>{
+    setTimeout(() => resolve('Promesa Rapida'), 1000);
+});
+
+export {
+    promesaLenta,
+    promesaMedia,
+    promesaRapida
+}
